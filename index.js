2ea04b76165824b4712983f184764ebda6387f9d
Julien Hnery <jwh_917@yahoo.com>
2022-01-05 08:05:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyCats = cats.slice();
+    copyCats.push("Broom");
+    return copyCats;
+}
+
+function prependCat(name){
+    const copyCats = cats.slice();
+    copyCats.unshift("Arnold");
+    return copyCats;
+}
+
+function removeLastCat(){
+    const copyCats = cats.slice();
+    copyCats.pop();
+    return copyCats;
+}
+
+function removeFirstCat(){
+    const copyCats = cats.slice();
+    copyCats.shift();
+    return copyCats;
+}
+
+
+    
+
